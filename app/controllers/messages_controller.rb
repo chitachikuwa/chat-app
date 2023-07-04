524d4678d4bf350f84bc4b4b62c86cdadadb347b
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-07-05 00:54:32 +0900

    メッセージ機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @Message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def carate
+    @room = Room.find(params[:room_id])
+    @message = @room.message.new(message_paramas)
+     if @message.save
+      redirect_to room_messasges_new_path(@room)
+     else
+      render :index 
+  end
+
+  private
+
+  def message_paramas
+   params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
