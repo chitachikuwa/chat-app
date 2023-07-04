53b4477ddeed48f8f88eb6de19e552680a451223
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-07-05 01:33:13 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,23 +2,22 @@ class MessagesController < ApplicationController
   def index
     @Message = Message.new
     @room = Room.find(params[:room_id])
-    @messages = room.messages.Include(:user)
-
+    @messages = @room.messages.includes(:user)
   end
 
-  def carate
+  def create
     @room = Room.find(params[:room_id])
-    @message = @room.message.new(message_paramas)
+    @message = @room.message.new(message_params)
      if @message.save
       redirect_to room_messasges_new_path(@room)
      else
-      @messages = @room.messages.Include(:user)
+      @messages = @room.messages.includes(:user)
       render :index 
   end
 
   private
 
-  def message_paramas
+  def message_params
    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
