f98cdbb10e45d4dfe13de819407ba1bf3cb1503a
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-07-10 00:16:47 +0900

    統合テストの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,8 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-     if @message.save
-      redirect_to room_messasges_new_path(@room)
+    if @message.save
+      redirect_to room_messages_path(@room)
      else
       @messages = @room.messages.includes(:user)
       render :index 
